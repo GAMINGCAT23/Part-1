eee8e1732a82ab1b985825f8f559c3ab66fa3972
GAMINGCAT23 <148483729+GAMINGCAT23@users.noreply.github.com>
2024-01-18 13:43:35 -0500

    1

--- Assets/Week 2/Scripts/Launcher.cs
+++ Assets/Week 2/Scripts/Launcher.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +7,7 @@ public class Launcher : MonoBehaviour
     public Transform Spawn;
     public GameObject missilePrefab;
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
         
     }
@@ -23,6 +22,4 @@ public class Launcher : MonoBehaviour
     {
         Instantiate(missilePrefab, Spawn.position, Spawn.rotation);
     }
-
-
 }
