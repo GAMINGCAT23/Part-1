a2609c1a64545ecf1af4f9020a21e6b5b2b9fc4f
GAMINGCAT23 <148483729+GAMINGCAT23@users.noreply.github.com>
2024-01-25 11:12:15 -0500

    Task 3 complete: car controller

--- Assets/Week 3/Scrpit/Car.cs
+++ Assets/Week 3/Scrpit/Car.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
-
-    float acceleration;
+public class Car : MonoBehaviour 
 {
+    float acceleration;
+    float steering;
+    public float fowardSpeed = 500;
+    public float steeringSpeed = 200;
+    Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        acceleration = Input.GetAxis("Vertical");
+        steering = Input.GetAxis("Horizontal");
     }
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(steering * -steeringSpeed * Time.deltaTime);
+        Vector2 force = transform.up * acceleration * fowardSpeed * Time.deltaTime;
+        rigidbody.AddForce(force);
+    }
+    
 }
