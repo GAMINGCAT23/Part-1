8277a9b11f154e35354f993cc2028ef0ac2d27be
GAMINGCAT23 <148483729+GAMINGCAT23@users.noreply.github.com>
2024-01-25 11:36:37 -0500

    Task 4a complete: finish line

--- Assets/Week 3/Scrpit/Car.cs
+++ Assets/Week 3/Scrpit/Car.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    float acceleration;
+    float steering;
+    public float fowardSpeed = 500;
+    public float steeringSpeed = 200;
+    Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        float acceleration;
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        acceleration = Input.GetAxis("Vertical");
+        steering = Input.GetAxis("Horizontal");
     }
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(steering * -steeringSpeed * Time.deltaTime);
+        Vector2 force = transform.up * acceleration * fowardSpeed * Time.deltaTime;
+        rigidbody.AddForce(force);
+    }
+
 }
